7b4c0e94ec5c7f12ad4377ea1f658c75cb71192d
tienganhpc2018 <tienganhpc2018@gmail.com>
2026-08-18 10:05:37 +0700

    Thêm mã nguồn: tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#f0fdf4',
+          100: '#dcfce7',
+          500: '#10b981',
+          600: '#059669',
+          700: '#047857',
+        },
+        navy: {
+          800: '#1e293b',
+          900: '#0f172a',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
